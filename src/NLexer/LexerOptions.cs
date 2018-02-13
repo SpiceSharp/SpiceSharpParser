23a8bfa6d72c79c1a8e0d8b10409cb5c92ddbeda
Marcin <xormus@gmail.com>
2018-02-13 18:41:03 +0100

    Adding comments to LexerOptions class

--- src/NLexer/LexerOptions.cs
+++ src/NLexer/LexerOptions.cs
@@ -2,10 +2,22 @@
 {
     public class LexerOptions
     {
+        /// <summary>
+        /// Specifies that all tokens' lexems are 'single-line'
+        /// </summary>
         public bool SingleLineTokens { get; set; } = false;
 
+        /// <summary>
+        /// Specifies that token's lexem can be 'multi-line'
+        /// </summary>
         public bool MultipleLineTokens { get; set; } = false;
 
+        /// <summary>
+        /// The character that makes next line to be part of current line
+        /// </summary>
+        /// <remarks>
+        /// In Spice netlist this is '+' character
+        /// </remarks>
         public char? LineContinuationCharacter { get; set; }
     }
 }
