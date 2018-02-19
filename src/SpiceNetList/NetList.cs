8ff7e0043b32e885e163ece2e4d193e1a5b86a1b
Marcin <xormus@gmail.com>
2018-02-19 22:06:12 +0100

    Adding comment for NetList

--- src/SpiceNetList/NetList.cs
+++ src/SpiceNetList/NetList.cs
@@ -2,10 +2,19 @@
 
 namespace SpiceNetlist
 {
+    /// <summary>
+    /// Spice netlist
+    /// </summary>
     public class NetList : SpiceObject
     {
+        /// <summary>
+        /// Gets or sets title of the netlist
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Gets or sets collection of statements
+        /// </summary>
         public Statements Statements { get; set; }
     }
 }
