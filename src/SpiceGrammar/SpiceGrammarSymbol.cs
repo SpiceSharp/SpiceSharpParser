44d710afc652416cad68bfea620178278857787c
Marcin <xormus@gmail.com>
2018-02-19 20:46:12 +0100

    Improving SpcieGrammar project: adding stylecop

--- src/SpiceGrammar/SpiceGrammarSymbol.cs
+++ src/SpiceGrammar/SpiceGrammarSymbol.cs
@@ -1,5 +1,8 @@
 ﻿namespace SpiceGrammar
 {
+    /// <summary>
+    /// Contains all non-terminals names for spice grammar from "SpiceBNF.txt"
+    /// </summary>
     public class SpiceGrammarSymbol
     {
         public const string START = "START";
@@ -16,19 +19,14 @@
         public const string PARAMETER_SINGLE = "PARAMETER-SINGLE";
         public const string PARAMETER_SINGLE_SEQUENCE = "PARAMETER-SINGLE-SEQUENCE";
         public const string PARAMETER_SINGLE_SEQUENCE_CONTINUE = "PARAMETER-SINGLE-SEQUENCE-CONTINUE";
-
         public const string VECTOR = "VECTOR";
-
         public const string VECTOR_CONTINUE = "VECTOR-CONTINUE";
-
         public const string PARAMETER_EQUAL = "PARAMETER_EQUAL";
         public const string PARAMETER_EQUAL_SINGLE = "PARAMETER_EQUAL_SINGLE";
         public const string PARAMETER_EQUAL_SEQUANCE = "PARAMETER_EQUAL_SEQUENCE";
         public const string PARAMETER_EQUAL_SEQUANCE_CONTINUE = "PARAMETER_EQUAL_SEQUENCE_CONTINUE";
-
         public const string PARAMETER_BRACKET = "PARAMETER_BRACKET";
         public const string PARAMETER_BRACKET_CONTENT = "PARAMETER_BRACKET_CONTENT";
-
         public const string NEW_LINE_OR_EOF = "NEW-LINE-OR-EOF";
     }
 }
