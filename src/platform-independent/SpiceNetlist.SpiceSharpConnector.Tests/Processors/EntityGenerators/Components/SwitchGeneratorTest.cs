7e8097643d5d7714fb5fdf1994a69ff3165700f1
Marcin <xormus@gmail.com>
2018-03-17 20:04:16 +0100

    Adding negative tests for SwitchGenerator

--- src/platform-independent/SpiceNetlist.SpiceSharpConnector.Tests/Processors/EntityGenerators/Components/SwitchGeneratorTest.cs
+++ src/platform-independent/SpiceNetlist.SpiceSharpConnector.Tests/Processors/EntityGenerators/Components/SwitchGeneratorTest.cs
@@ -38,6 +38,31 @@ namespace SpiceNetlist.SpiceSharpConnector.Tests.Processors.EntityGenerators.Com
         }
 
         [Fact]
+        public void GenerateVoltageSwitchOff()
+        {
+            var evaluator = new Evaluator();
+            var context = Substitute.For<IProcessingContext>();
+            context.FindModel<VoltageSwitchModel>(Arg.Any<string>()).Returns(new VoltageSwitchModel("SModel"));
+
+            var parameters = new ParameterCollection
+            {
+                new ValueParameter("1"),
+                new ValueParameter("0"),
+                new ValueParameter("2"),
+                new ValueParameter("0"),
+                new WordParameter("model"),
+                new WordParameter("Off")
+            };
+
+            var generator = new SwitchGenerator();
+            var @switch = generator.Generate(new SpiceSharp.Identifier("s1"), "s1", "s", parameters, context);
+
+            Assert.NotNull(@switch);
+            Assert.IsType<VoltageSwitch>(@switch);
+            Assert.False(@switch.ParameterSets.Get<SpiceSharp.Components.VoltageSwitchBehaviors.BaseParameters>().ZeroState);
+        }
+
+        [Fact]
         public void GenerateCurrentSwitch()
         {
             var evaluator = new Evaluator();
@@ -59,6 +84,30 @@ namespace SpiceNetlist.SpiceSharpConnector.Tests.Processors.EntityGenerators.Com
             Assert.NotNull(@switch);
             Assert.IsType<CurrentSwitch>(@switch);
             Assert.True(@switch.ParameterSets.Get<SpiceSharp.Components.CurrentSwitchBehaviors.BaseParameters>().ZeroState);
+        }
+
+        [Fact]
+        public void GenerateCurrentSwitchOff()
+        {
+            var evaluator = new Evaluator();
+            var context = Substitute.For<IProcessingContext>();
+            context.FindModel<CurrentSwitchModel>(Arg.Any<string>()).Returns(new CurrentSwitchModel("WModel"));
+
+            var parameters = new ParameterCollection
+            {
+                new ValueParameter("1"),
+                new ValueParameter("0"),
+                new IdentifierParameter("V3"),
+                new WordParameter("model"),
+                new WordParameter("Off")
+            };
+
+            var generator = new SwitchGenerator();
+            var @switch = generator.Generate(new SpiceSharp.Identifier("w1"), "w1", "w", parameters, context);
+
+            Assert.NotNull(@switch);
+            Assert.IsType<CurrentSwitch>(@switch);
+            Assert.False(@switch.ParameterSets.Get<SpiceSharp.Components.CurrentSwitchBehaviors.BaseParameters>().ZeroState);
         }
     }
 }
