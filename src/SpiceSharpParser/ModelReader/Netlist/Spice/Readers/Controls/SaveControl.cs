d5f6dd5d65eeb384a6f2a80a6ba4ca805990c67f
Marcin <xormus@gmail.com>
2018-06-03 11:05:46 +0200

    Very tine cleanup

--- src/SpiceSharpParser/ModelReader/Netlist/Spice/Readers/Controls/SaveControl.cs
+++ src/SpiceSharpParser/ModelReader/Netlist/Spice/Readers/Controls/SaveControl.cs
@@ -43,43 +43,7 @@ namespace SpiceSharpParser.ModelReader.Netlist.Spice.Readers.Controls
 
             if (statement.Parameters.Count == 0)
             {
-                context.Result.Circuit.Objects.BuildOrderedComponentList(); //TODO: Verify with Sven
-
-                // For all simulations add exports for current and voltages
-                foreach (var simulation in context.Result.Simulations)
-                {
-                    var nodes = new List<Identifier>();
-
-                    foreach (Entity entity in context.Result.Circuit.Objects)
-                    {
-                        if (entity is SpiceSharp.Components.Component c)
-                        {
-                            string componentName = c.Name.ToString();
-                            var @params = new ParameterCollection();
-                            @params.Add(new WordParameter(componentName));
-
-                            for (var i = 0; i < c.PinCount; i++)
-                            {
-                                var node = c.GetNode(i);
-                                if (!nodes.Contains(node))
-                                {
-                                    nodes.Add(node);
-                                }
-                            }
-
-                            // Add current export for component
-                            context.Result.AddExport(Registry.Get("i").CreateExport("i", @params, simulation, context));
-                        }
-                    }
-
-                    foreach (var node in nodes)
-                    {
-                        var @params = new ParameterCollection();
-                        @params.Add(new WordParameter(node.ToString()));
-
-                        context.Result.AddExport(Registry.Get("v").CreateExport("v", @params, simulation, context));
-                    }
-                }
+                CreateExportsForAllVoltageAndCurrents(context);
             }
 
             for (var i = 0; i < statement.Parameters.Count; i++)
@@ -112,6 +76,47 @@ namespace SpiceSharpParser.ModelReader.Netlist.Spice.Readers.Controls
                 else if ((i != 0 || (i == 0 && simulationType == null)) && parameter is SingleParameter s)
                 {
                     AddLetExport(context, simulationType, s);
+                }
+            }
+        }
+
+        private void CreateExportsForAllVoltageAndCurrents(IReadingContext context)
+        {
+            context.Result.Circuit.Objects.BuildOrderedComponentList(); //TODO: Verify with Sven
+
+            // For all simulations add exports for current and voltages
+            foreach (var simulation in context.Result.Simulations)
+            {
+                var nodes = new List<Identifier>();
+
+                foreach (Entity entity in context.Result.Circuit.Objects)
+                {
+                    if (entity is SpiceSharp.Components.Component c)
+                    {
+                        string componentName = c.Name.ToString();
+                        var @params = new ParameterCollection();
+                        @params.Add(new WordParameter(componentName));
+
+                        for (var i = 0; i < c.PinCount; i++)
+                        {
+                            var node = c.GetNode(i);
+                            if (!nodes.Contains(node))
+                            {
+                                nodes.Add(node);
+                            }
+                        }
+
+                        // Add current export for component
+                        context.Result.AddExport(Registry.Get("i").CreateExport("i", @params, simulation, context));
+                    }
+                }
+
+                foreach (var node in nodes)
+                {
+                    var @params = new ParameterCollection();
+                    @params.Add(new WordParameter(node.ToString()));
+
+                    context.Result.AddExport(Registry.Get("v").CreateExport("v", @params, simulation, context));
                 }
             }
         }
