eb3a02620f20f94cf07c14b6c5fc0a629621ebeb
Marcin <xormus@gmail.com>
2018-10-23 19:46:18 +0200

    Removing value point syntax + integration tests for F|H

--- src/SpiceSharpParser.IntegrationTests/Components/VoltageControlledSourcesTests.cs
+++ src/SpiceSharpParser.IntegrationTests/Components/VoltageControlledSourcesTests.cs
@@ -2,14 +2,6 @@
 
 namespace SpiceSharpParser.IntegrationTests.Components
 {
-    using System;
-
-    using SpiceSharp;
-    using SpiceSharp.Components;
-    using SpiceSharp.Simulations;
-
-    using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.Controls.Exporters.CurrentExports;
-
     public class VoltageControlledSourcesTests : BaseTests
     {
         [Fact]
@@ -20,23 +12,6 @@ namespace SpiceSharpParser.IntegrationTests.Components
                 "V1 1 0 100",
                 "R1 1 0 10",
                 "E1 (2, 0) (1, 0) 1.5",
-                ".SAVE V(2,0)",
-                ".OP",
-                ".END");
-
-            var export = RunOpSimulation(netlist, "V(2,0)");
-            Assert.NotNull(netlist);
-            Assert.Equal(150, export);
-        }
-
-        [Fact]
-        public void VoltageControlledVoltageSourceWithValuePointFormat()
-        {
-            var netlist = ParseNetlist(
-                "Voltage controlled voltage source - point format",
-                "V1 1 0 100",
-                "R1 1 0 10",
-                "E1 2 0 1 0 (1.5)",
                 ".SAVE V(2,0)",
                 ".OP",
                 ".END");
@@ -62,24 +37,6 @@ namespace SpiceSharpParser.IntegrationTests.Components
             var export = RunOpSimulation(netlist, "I(R2)");
             Assert.NotNull(netlist);
             Assert.Equal(-150, export);
-        }
-
-        [Fact]
-        public void VoltageControlledCurrentSourceWithValuePointFormat()
-        {
-            var netlist = ParseNetlist(
-                "Voltage controlled current source - point format",
-                "V1 1 0 200",
-                "R1 1 0 10",
-                "G1 2 0 1 0 (1.5)",
-                "R2 2 0 100",
-                ".SAVE I(R2)",
-                ".OP",
-                ".END");
-
-            var export = RunOpSimulation(netlist, "I(R2)");
-            Assert.NotNull(netlist);
-            Assert.Equal(-300, export);
         }
 
         [Fact]
