24794999e6d6ef92ad344bc0c5d855b56b5b4f3d
Marcin <xormus@gmail.com>
2018-11-04 18:58:33 +0100

    Fixing a test for Voltage Delay

--- src/SpiceSharpParser.IntegrationTests/Components/VoltageDelayTests.cs
+++ src/SpiceSharpParser.IntegrationTests/Components/VoltageDelayTests.cs
@@ -4,13 +4,13 @@ namespace SpiceSharpParser.IntegrationTests.Components
 {
     public class VoltageDelayTests : BaseTests
     {
-        //[Fact]
+        [Fact]
         public void PulseWithoutBracket()
         {
             var netlist = ParseNetlist(
                 "Voltage delay",
                 "V1 1 0 SINE(0 5 50 0 0 90)",
-                "BVD1 1 0 2 0 1e-2",
+                "BVD1 2 0 1 0 1e-2",
                 "R1 1 0 10",
                 "R2 2 0 10",
                 ".SAVE V(1,0)",
