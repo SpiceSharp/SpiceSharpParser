dc16957bfc4e0769f99cb01e2ec47a5286481f65
Marcin <xormus@gmail.com>
2018-11-12 12:31:16 +0100

    Enhancing poly for E|G|F|H -> POLY(1) == POLY

--- src/SpiceSharpParser.IntegrationTests/AnalogBehavioralModeling/PolyTests.cs
+++ src/SpiceSharpParser.IntegrationTests/AnalogBehavioralModeling/PolyTests.cs
@@ -5,6 +5,23 @@ namespace SpiceSharpParser.IntegrationTests.AnalogBehavioralModeling
     public class PolyTests : BaseTests
     {
         [Fact]
+        public void VoltageControlledVoltageSourceFormatWithoutDimension()
+        {
+            var netlist = ParseNetlist(
+                "Poly(1) E test circuit - first format",
+                "R1 1 0 100",
+                "V1 1 0 2",
+                "ESource 2 0 POLY 1 0 2 1", // V(1) + 2
+                ".OP",
+                ".SAVE V(2,0)",
+                ".END");
+
+            Assert.NotNull(netlist);
+            double export = RunOpSimulation(netlist, "V(2,0)");
+            Assert.Equal(4, export);
+        }
+
+        [Fact]
         public void VoltageControlledVoltageSourceFirstFormat()
         {
             var netlist = ParseNetlist(
@@ -73,6 +90,22 @@ namespace SpiceSharpParser.IntegrationTests.AnalogBehavioralModeling
             double export = RunOpSimulation(netlist, "V(2,0)");
             Assert.Equal(7, export);
         }
+        [Fact]
+        public void VoltageControlledCurrentSourceFormatWithoutDimension()
+        {
+            var netlist = ParseNetlist(
+                "Poly(1) G test circuit",
+                "R1 1 0 100",
+                "V1 2 0 2",
+                "GSource 1 0 POLY 2 0 2 1", // V(2) + 2
+                ".OP",
+                ".SAVE I(GSource)",
+                ".END");
+
+            Assert.NotNull(netlist);
+            double export = RunOpSimulation(netlist, "I(GSource)");
+            Assert.Equal(4, export);
+        }
 
         [Fact]
         public void VoltageControlledCurrentSourceFirstFormat()
@@ -127,6 +160,24 @@ namespace SpiceSharpParser.IntegrationTests.AnalogBehavioralModeling
         }
 
         [Fact]
+        public void CurrentControlledCurrentSourceWithoutDimension()
+        {
+            var netlist = ParseNetlist(
+                "Poly(1) F test circuit",
+                "R1 1 0 100",
+                "R2 1 0 200",
+                "I1 1 0 2",
+                "FSource 2 0 POLY I1 2 1", // I(I1) + 2 
+                ".OP",
+                ".SAVE I(FSource)",
+                ".END");
+
+            Assert.NotNull(netlist);
+            double export = RunOpSimulation(netlist, "I(FSource)");
+            Assert.Equal(4, export);
+        }
+
+        [Fact]
         public void CurrentControlledVoltageSource()
         {
             var netlist = ParseNetlist(
@@ -134,6 +185,23 @@ namespace SpiceSharpParser.IntegrationTests.AnalogBehavioralModeling
                 "R1 1 0 100",
                 "I1 1 0 2",
                 "HSource 2 0 POLY(1) I1 2 1", // I(I1) + 2 
+                ".OP",
+                ".SAVE V(2,0)",
+                ".END");
+
+            Assert.NotNull(netlist);
+            double export = RunOpSimulation(netlist, "V(2,0)");
+            Assert.Equal(4, export);
+        }
+
+        [Fact]
+        public void CurrentControlledVoltageSourceWithoutDimension()
+        {
+            var netlist = ParseNetlist(
+                "Poly(1) H test circuit",
+                "R1 1 0 100",
+                "I1 1 0 2",
+                "HSource 2 0 POLY I1 2 1", // I(I1) + 2 
                 ".OP",
                 ".SAVE V(2,0)",
                 ".END");
