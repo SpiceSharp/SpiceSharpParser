a41c428551934f08605fed9940947a723a6623f3
Marcin <xormus@gmail.com>
2018-11-20 21:56:20 +0100

    Additional test for a capacitor when only TC1 is provided

--- src/SpiceSharpParser.IntegrationTests/Components/CapacitorTests.cs
+++ src/SpiceSharpParser.IntegrationTests/Components/CapacitorTests.cs
@@ -95,5 +95,29 @@ namespace SpiceSharpParser.IntegrationTests.Components
             Func<double, double> reference = t => dcVoltage * (1.0 - Math.Exp(-t / tau));
             EqualsWithTol(exports, reference);
         }
+
+        [Fact]
+        public void When_TemperatureDependentCapacitorOnlyTC1_Expect_Reference()
+        {
+            double dcVoltage = 10;
+            double resistorResistance = 10e3; // 10000;
+            double capacitance = 1e-6 * (1 + 1.0 * 3.0); // 0.000001;
+            double tau = resistorResistance * capacitance;
+
+            var netlist = ParseNetlist(
+                "Capacitor circuit - The initial voltage on capacitor is 0V. The result should be an exponential converging to dcVoltage.",
+                "C1 OUT 0 1e-6 TC=1.0",
+                "R1 IN OUT 10e3",
+                "V1 IN 0 10",
+                ".IC V(OUT)=0.0",
+                ".TRAN 1e-8 10e-6",
+                ".SAVE V(OUT)",
+                ".OPTIONS TEMP = 30",
+                ".END");
+
+            var exports = RunTransientSimulation(netlist, "V(OUT)");
+            Func<double, double> reference = t => dcVoltage * (1.0 - Math.Exp(-t / tau));
+            EqualsWithTol(exports, reference);
+        }
     }
 }
