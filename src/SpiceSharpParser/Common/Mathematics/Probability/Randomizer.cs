0e0274e20e5623f67c18846bfd79fbbdebd27936
Marcin <xormus@gmail.com>
2019-06-25 22:02:26 +0200

    Code re-use

--- src/SpiceSharpParser/Common/Mathematics/Probability/Randomizer.cs
+++ src/SpiceSharpParser/Common/Mathematics/Probability/Randomizer.cs
@@ -16,46 +16,12 @@ namespace SpiceSharpParser.Common.Mathematics.Probability
 
         public IRandomDoubleProvider GetRandomDoubleProvider(int? seed, string pdfName = null)
         {
-            if (pdfName == null)
-            {
-                return _defaultRandomNumberProviderFactory.GetRandomDouble(seed);
-            }
-
-            if (_pdfDictionary.ContainsKey(pdfName))
-            {
-                if (!_customRandomNumberProviderFactories.ContainsKey(pdfName))
-                {
-                    _customRandomNumberProviderFactories[pdfName] = new CustomRandomNumberProviderFactory(_pdfDictionary[pdfName]);
-                }
-
-                return _customRandomNumberProviderFactories[pdfName].GetRandomDouble(seed);
-            }
-            else
-            {
-                throw new ArgumentException("Unknown pdf", nameof(pdfName));
-            }
+            return GetRandomProvider(seed, pdfName);
         }
 
         public IRandomIntegerProvider GetRandomIntegerProvider(int? seed, string pdfName = null)
         {
-            if (pdfName == null)
-            {
-                return _defaultRandomNumberProviderFactory.GetRandomInteger(seed);
-            }
-
-            if (_pdfDictionary.ContainsKey(pdfName))
-            {
-                if (!_customRandomNumberProviderFactories.ContainsKey(pdfName))
-                {
-                    _customRandomNumberProviderFactories[pdfName] = new CustomRandomNumberProviderFactory(_pdfDictionary[pdfName]);
-                }
-
-                return _customRandomNumberProviderFactories[pdfName].GetRandomInteger(seed);
-            }
-            else
-            {
-                throw new ArgumentException("Unknown pdf", nameof(pdfName));
-            }
+            return GetRandomProvider(seed, pdfName);
         }
 
         public IRandomNumberProvider GetRandomProvider(int? seed, string pdfName = null)
