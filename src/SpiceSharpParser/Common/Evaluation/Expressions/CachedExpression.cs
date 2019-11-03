8fb10f32526f12e00505820614389d9e099fe787
Marcin <xormus@gmail.com>
2019-11-03 15:08:02 +0100

    Revert "Remove CachedExpression"
    This reverts commit 93d7d935fe5ab57776fdedffe980d8a1a93a8648.

---
+++ src/SpiceSharpParser/Common/Evaluation/Expressions/CachedExpression.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using SpiceSharp.Simulations;
+using SpiceSharpParser.ModelReaders.Netlist.Spice.Context;
+
+namespace SpiceSharpParser.Common.Evaluation.Expressions
+{
+    public class CachedExpression : Expression
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachedExpression"/> class.
+        /// </summary>
+        /// <param name="expression">Expression string.</param>
+        public CachedExpression(string expression)
+            : base(expression)
+        {
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether value of cached expression has been computed.
+        /// </summary>
+        protected bool IsEvaluated { get; private set; }
+
+        /// <summary>
+        /// Evaluates the expression.
+        /// </summary>
+        /// <param name="evaluator">Evaluator.</param>
+        /// <param name="context">Context.</param>
+        /// <param name="sim">Simulation.</param>
+        /// <param name="readingContext">Reading context.</param>
+        /// <returns>
+        /// The value of the expression.
+        /// </returns>
+        public override double Evaluate(IEvaluator evaluator, ExpressionContext context, Simulation sim, IReadingContext readingContext)
+        {
+            if (evaluator == null)
+            {
+                throw new ArgumentNullException(nameof(evaluator));
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (!IsEvaluated)
+            {
+                CurrentValue = evaluator.EvaluateValueExpression(ValueExpression,context, sim, readingContext);
+                IsEvaluated = true;
+            }
+
+            return CurrentValue;
+        }
+
+        /// <summary>
+        /// Invalidate the expression.
+        /// </summary>
+        public override void Invalidate()
+        {
+            IsEvaluated = false;
+        }
+
+        /// <summary>
+        /// Clones the cached expression.
+        /// </summary>
+        /// <returns>
+        /// A cloned cached expression.
+        /// </returns>
+        public override Expression Clone()
+        {
+            return new CachedExpression(ValueExpression) { CurrentValue = CurrentValue, IsEvaluated = true };
+        }
+    }
+}
