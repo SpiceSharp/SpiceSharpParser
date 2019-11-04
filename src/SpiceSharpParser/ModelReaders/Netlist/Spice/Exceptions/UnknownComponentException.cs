7d4a9439482b5623a092dc426119b29007bde1a9
Marcin <xormus@gmail.com>
2019-11-04 06:06:47 +0100

    Add better exceptions

---
+++ src/SpiceSharpParser/ModelReaders/Netlist/Spice/Exceptions/UnknownComponentException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SpiceSharpParser.ModelReaders.Netlist.Spice.Exceptions
+{
+    /// <summary>
+    /// Unknown component exception.
+    /// </summary>
+    public class UnknownComponentException : Exception
+    {
+        public UnknownComponentException()
+        {
+        }
+
+        public UnknownComponentException(string message)
+            : base(message)
+        {
+        }
+
+        public UnknownComponentException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
