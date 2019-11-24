bba865bbaf74600a937f9bdde00e4b2c12b55005
Marcin <xormus@gmail.com>
2019-11-24 17:08:48 +0100

    Fix travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -4,8 +4,10 @@ cd src
 echo "Executing MSBuild DLL begin command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger:trx
+dotnet build SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj
+dotnet build SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj
 echo "Running tests..."
+dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger:trx
 dotnet test SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj --logger:trx
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
