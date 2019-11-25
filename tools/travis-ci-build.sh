649fca940210140faf5b104dd52d193a7bda923b
Marcin <xormus@gmail.com>
2019-11-25 22:29:48 +0100

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,10 +2,10 @@
 echo "Changing to /src directory..."
 cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="*/TestResults/.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="*/TestResults/*.trx" /d:sonar.cs.vscoveragexml.reportsPaths="./TestResults/*.xml"	 /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build
 echo "Running tests..."
-dotnet test --logger:trx
+dotnet test --logger:trx --results-directory:"./TestResults" --collect:"Code Coverage"
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
