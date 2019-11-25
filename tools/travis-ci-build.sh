f3b152a6a71900f19f3b7f9d314995d6841fb011
Marcin <xormus@gmail.com>
2019-11-25 20:48:14 +0100

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,13 +2,13 @@
 echo "Changing to /src directory..."
 cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="Tests.trx;IntegrationTests.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj
 dotnet build SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj
 echo "Running tests..."
-dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger:trx
-dotnet test SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj --logger:trx
+dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger:trx "trx;LogFileName=Tests.trx"
+dotnet test SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj --logger "trx;LogFileName=IntegrationTests.trx"
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
 
