c9a7c86f290b1d22bd562a309f8b4bad57081869
Marcin <xormus@gmail.com>
2019-11-25 20:29:00 +0100

    Try a new path to results of tests

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,7 +2,7 @@
 echo "Changing to /src directory..."
 cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="**/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj
 dotnet build SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj
