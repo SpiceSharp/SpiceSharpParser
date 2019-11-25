e26ae81162394477f92d599da8aa3e7c7e453beb
Marcin <xormus@gmail.com>
2019-11-25 20:52:45 +0100

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -7,7 +7,7 @@ echo "Running build..."
 dotnet build SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj
 dotnet build SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj
 echo "Running tests..."
-dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger:trx "trx;LogFileName=Tests.trx"
+dotnet test SpiceSharpParser.Tests/SpiceSharpParser.Tests.csproj --logger "trx;LogFileName=Tests.trx"
 dotnet test SpiceSharpParser.IntegrationTests/SpiceSharpParser.IntegrationTests.csproj --logger "trx;LogFileName=IntegrationTests.trx"
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
