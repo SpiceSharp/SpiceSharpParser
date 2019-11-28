9e7fa8278d83bccf737092d0bfc312835ed8b57b
Marcin <xormus@gmail.com>
2019-11-28 20:23:24 +0100

    Attemp to enable code coverage in sonar cloud

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,10 +2,10 @@
 echo "Changing to /src directory...."
 cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.cs.vstest.reportsPaths="*/TestResults/*.trx" /d:sonar.cs.vscoveragexml.reportsPaths="./TestResults/*.xml"	 /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"marcin-golebiowski" /k:"SpiceSharpParser" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN} /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.cs.opencover.reportsPaths="lcov.opencover.xml"
 echo "Running build..."
 dotnet build
 echo "Running tests..."
-dotnet test --logger:trx --results-directory:"./TestResults" --collect:"Code Coverage"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
 echo "Executing MSBuild DLL end command..."
 dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
