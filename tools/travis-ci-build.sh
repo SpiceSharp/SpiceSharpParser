ba3b385ab1f7044a9ea15ccff10998c7c2a600e1
Marcin <xormus@gmail.com>
2021-04-26 17:38:10 +0200

    Delete travis-ci-build.sh

--- tools/travis-ci-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Changing to /src directory...."
-cd src
-echo "Running build..."
-dotnet build
-echo "Running tests..."
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
-
