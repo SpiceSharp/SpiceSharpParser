6d731c72dc00e6864b70b12664962e50995cac85
Marcin Golebiowski <xormus@gmail.com>
2021-05-05 20:00:33 +0200

    Add additional test for subcricuits when expand = false and there are subcircuit parameters

--- src/SpiceSharpParser.IntegrationTests/Examples/Example01.cs
+++ src/SpiceSharpParser.IntegrationTests/Examples/Example01.cs
@@ -13,6 +13,7 @@ namespace SpiceSharpParser.IntegrationTests.Examples
 
             var parser = new SpiceParser();
             parser.Settings.Lexing.HasTitle = true;
+            parser.Settings.Reading.ExpandSubcircuits = true;
 
             var parseResult = parser.ParseNetlist(netlistContent);
 
@@ -27,6 +28,24 @@ namespace SpiceSharpParser.IntegrationTests.Examples
         public void When_Simulated_2_Expect_NoExceptions()
         {
             string path = Path.Combine(Directory.GetCurrentDirectory(), "Examples/Circuits/example01_without_params.cir");
+            var netlistContent = File.ReadAllText(path);
+
+            var parser = new SpiceParser();
+            parser.Settings.Lexing.HasTitle = true;
+            parser.Settings.Reading.ExpandSubcircuits = false;
+
+            var parseResult = parser.ParseNetlist(netlistContent);
+
+            double[] exports = RunOpSimulation(parseResult.SpiceModel, new[] { "V(N1)", "V(N2)", "V(N3)" });
+
+            EqualsWithTol(1.0970919064909939, exports[0]);
+            EqualsWithTol(0.014696545624995935, exports[1]);
+            EqualsWithTol(0.014715219080886419, exports[2]);
+        }
+        [Fact]
+        public void When_Simulated_3_Expect_NoExceptions()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Examples/Circuits/example01.cir");
             var netlistContent = File.ReadAllText(path);
 
             var parser = new SpiceParser();
