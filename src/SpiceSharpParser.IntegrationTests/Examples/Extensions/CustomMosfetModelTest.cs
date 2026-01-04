ea4e092b78643377c4db31d24cc679efd51c5f8b
Marcin Gołębiowski <xormus@gmail.com>
2026-01-04 19:00:02 +0100

    add SpiceSharpBSIM

--- src/SpiceSharpParser.IntegrationTests/Examples/Extensions/CustomMosfetModelTest.cs
+++ src/SpiceSharpParser.IntegrationTests/Examples/Extensions/CustomMosfetModelTest.cs
@@ -1,4 +1,5 @@
-﻿using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.EntityGenerators.Components.Semiconductors;
+﻿using SpiceSharp.Components;
+using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.EntityGenerators.Components.Semiconductors;
 using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.EntityGenerators.Models;
 using System.IO;
 using Xunit;
@@ -43,6 +44,36 @@ namespace SpiceSharpParser.IntegrationTests.Examples.Extensions
             // custom mosfet models
             var modelGenerator = new MosfetModelGenerator();
             modelGenerator.AddLevel<SpiceSharp.Components.Mosfet3Model, SpiceSharp.Components.Mosfets.Level3.ModelParameters>(4);
+            spiceSharpReader.Settings.Mappings.Models.Map(new[] { "PMOS", "NMOS" }, modelGenerator);
+            var mosfetGenerator = new MosfetGenerator();
+            mosfetGenerator.AddMosfet<SpiceSharp.Components.Mosfet3Model, SpiceSharp.Components.Mosfet3>();
+            spiceSharpReader.Settings.Mappings.Components.Map("M", mosfetGenerator);
+
+
+            var spiceSharpModel = spiceSharpReader.Read(parseResult.FinalModel);
+
+            Assert.False(spiceSharpModel.ValidationResult.HasError);
+            Assert.False(spiceSharpModel.ValidationResult.HasWarning);
+        }
+
+
+        [Fact]
+        public void When_BSIM1_Used_NoExceptions()
+        {
+            // Create a model from text file
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Examples/Circuits/MosfetExample2.cir");
+            var netlistContent = File.ReadAllText(path);
+            var parser = new SpiceNetlistParser();
+            parser.Settings.Lexing.HasTitle = true;
+            var parseResult = parser.ParseNetlist(netlistContent);
+
+            // Convert to Spice#
+            var spiceSharpReader = new SpiceSharpReader();
+            spiceSharpReader.Settings.CaseSensitivity.IsModelTypeCaseSensitive = false;
+
+            // custom mosfet models
+            var modelGenerator = new MosfetModelGenerator();
+            modelGenerator.AddGenericLevel<BSIM1Model, SpiceSharpBSIM.Components.Semiconductors.BSIM.BSIM1Behaviors.ModelParameters>(4);
             spiceSharpReader.Settings.Mappings.Models.Map(new[] { "PMOS", "NMOS" }, modelGenerator);
             var mosfetGenerator = new MosfetGenerator();
             mosfetGenerator.AddMosfet<SpiceSharp.Components.Mosfet3Model, SpiceSharp.Components.Mosfet3>();
